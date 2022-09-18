d4eb431d0b34f7abc318e20747b8b8af21986ba8
Ahmed Alamin <engahmedegypto@gmail.com>
2022-09-18 11:07:38 +0200

    Finished Chapter 3

--- app/src/main/java/com/ahmedalamin/practcingkotlin/HeadFirstKotlinBook.kt
+++ app/src/main/java/com/ahmedalamin/practcingkotlin/HeadFirstKotlinBook.kt
@@ -65,5 +65,26 @@ variable or evaluating an expression from inside a String.
 -chapter 3
 (BULLET POINTS)
 
+*Use functions to organize your code and make it more reusable.
+*A function can have parameters, so you can pass more than one
+value to it.
+*The number and type of values you pass to the function must match
+the order and type of the parameters declared by the function.
+*A function can return a value. You must define the type of value (if
+any) it returns.
+*A Unit return type means that the function doesn’t return anything.
+Choose for loops over while loops when you know how many
+times you want to repeat the loop code.
+*The readLine() function reads a line of input from the standard
+*input stream. It returns a String value, the text entered by the user.
+*If the input stream has been redirected to a file and the end of the
+file has been reached, the readLine() function returns null. null
+means it has no value, or it’s missing.
+*&& means “and”. || means “or”. ! means “not”.
+
+
+
+
+
 
  */
--- app/src/main/java/com/ahmedalamin/practcingkotlin/TestingCode.kt
+++ app/src/main/java/com/ahmedalamin/practcingkotlin/TestingCode.kt
@@ -4,6 +4,16 @@ fun main() {
 
     println("hello this is ahmed")
 
+   val arr = arrayOf(2,3)
+
+    val arr2 = arrayOf(2,2,2)
+
+
+    arr[1] = 4
+
+    println(arr[1])
     //Testing committing and pushing to Github
 }
+
+
 
